bdc914f0ec196913100167703ba7ed2f3b75b585
unknown <nijunfeng@ESG.360ES.CN>
2024-10-30 17:33:46 +0800

    leetcode add

--- leetcode22.py
+++ leetcode22.py
@@ -1,28 +1,33 @@
-def judge_list(l) -> bool:
-    stack = []
-    mapping = {')':'('}
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def judge_str(l):
+            stack = []
+            mapping = {')':'('}
 
-    for i in l:
-        if i == '(':
-            stack.append(i)
-        #注意条件
-        elif i in mapping and stack and mapping[i] == stack[-1]:
-            stack.pop()
-        else:
-            return False
-    
-    return not stack
+            for i in l:
+                if i == '(':
+                    stack.append(i)
+                elif i in mapping and stack and mapping[i] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            
+            return not stack
 
-res = []
-def dfs(l,n,index):
-    if index == 2*n-1:
-        if judge_list(l):
-            res.append(l[:])
-        return 
-    
-    
-    dfs(l.append('('),n,index+1)
-    dfs(l.append(')'),n,index+1)
-
-
-dfs
+        
+        def dfs(l, n, index):
+            if index == 2 * n:
+                if judge_str(l):
+                    res.append(l)
+                return
+            
+            dfs(l + '(', n, index + 1)
+            dfs(l + ')', n, index + 1)
+        
+        res = []
+        dfs("", n, 0)
+        return res
