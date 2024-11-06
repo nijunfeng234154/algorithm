ab1e00c3e4e8d5a805a77874fb1bb607b9bce5c7
njf234154 <1162986505@qq.com>
2024-11-06 18:27:27 +0800

    new file:   leetcode27.py 	renamed:    leetcode9 -> leetcode9.py

---
+++ leetcode27.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        res = [i for i in nums if i!=val] 
+        
+        return len(res)
