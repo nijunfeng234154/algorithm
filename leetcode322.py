376748698ca6f420ad5af350aece22f409597aa5
nibapi <43716186+nijunfeng234154@users.noreply.github.com>
2024-11-11 19:48:46 +0800

    Update leetcode322.py

--- leetcode322.py
+++ leetcode322.py
@@ -5,12 +5,17 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        f = [10000]*(len(coins)+5)
+        #初始化都是初始化为总价值+1
+        f = [1e8]*(amount+1)
+        
+        f[0] = 0
         #f[i]表示从0到i这个硬币这些位置所有选择满足amount集合的最小值
-        summ = 0
-        for i in range(len(coins)):
-            if summ-coins[i] == amount:
-                f[i] = f[i-1]+1
-            else:
-                summ += coins[i]
-            
+        #完全背包，把问题看成每个硬币价值为1，体积为金额
+        for i in coins:
+            for j in range(i,amount+1):
+                f[j] = min(f[j],f[j-i]+1)
+        
+        if f[amount] == 1e8:
+            return -1
+        
+        return f[amount]
