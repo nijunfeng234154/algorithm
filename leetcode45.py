0d50c2b91d73c89e7e664908467e44025b996713
njf234154 <1162986505@qq.com>
2024-11-14 09:57:53 +0800

    leetcode45 待优化

---
+++ leetcode45.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = []
+        def dfs(nums,path,cnt):
+            if path == len(nums)-1:
+                res.append(cnt)
+                return 
+            if path >= len(nums):
+                return 
+
+            #搜索每一步要跳多少个位置
+            for i in range(1,nums[path]+1):
+                path = i+path
+                cnt += 1
+                dfs(nums,path,cnt)
+                cnt -= 1
+                path = 0
+
+        dfs(nums,0,0) 
+        return min(res) 
+
+sol = Solution()
+print(sol.jump([2,3,1,1,4]))     
+print(sol.jump([2,3,0,1,4]))     
+
+                
